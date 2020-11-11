65faa06f5b53ed3f7f4d8f91eefcc71b8c6d4a01
Pedro Neto <pedro@tuxcuiabano.com>
2020-11-11 03:47:21 -0400

    primeiro submit

---
+++ livraria/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Livro(models.Model):
+    autor = models.CharField(max_length=50)
+    editora = models.CharField(max_length=50)
+    isbn = models.IntegerField()
+    numeroPaginas = models.IntegerField()
+    titulo = models.CharField(max_length=50)
+    anoPublicacao = models.IntegerField()
+    emailEditora = models.EmailField()
---
+++ livraria/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from .views import *
+
+urlpatterns = [
+    url(r'^livro_list/', livro_list, name='livro_list'),
+    url(r'^livro_new/', livro_new, name='livro_new'),
+    url(r'^livro_edit/(?P<pk>[0-9]+)', livro_edit, name='livro_edit'),
+    url(r'^livro_remove/(?P<pk>[0-9]+)', livro_remove, name='livro_remove')
+
+
+]
